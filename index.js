8643239096248b56df528f03e8d51ffc5129a17d
Elad Yashar <eladyashar1104@gmail.com>
2024-01-30 12:54:33 +0200

    add class route

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World! - Elad new server' + process.env.STAM)
 })
 
+app.get('/class', (req, res) => {
+    res.send('Hello class!')
+  })
+  
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
